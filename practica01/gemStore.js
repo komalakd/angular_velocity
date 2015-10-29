f8a4f2392fbaed26a0e4ef0ad1832c005c0bac0c
Andres Valle <andresvalle.isi@gmail.com>
2015-10-29 00:54:30 -0300

    Se finalizo la practica01.

--- practica01/gemStore.js
+++ practica01/gemStore.js
@@ -1,13 +1,10 @@
-var gem = { name: 'Azurite', price: 2.95 };
-
-var gems = [
-    { name: 'Azurite', price: 2.75 },
-    { name: 'Bloodstone', price: 5.95 },
-    { name: 'Zircon', price: 3.95 }
-];
-
 angular
     .module("gemStore",[])
     .controller("StoreController", function(){
-        this.gem = gem
+        this.gem = { name: 'Azurite', price: 2.95, canPurchase: true };
+        this.gems = [
+            { name: 'Azurite', price: 2.75 },
+            { name: 'Bloodstone', price: 5.95 },
+            { name: 'Zircon', price: 3.95 }
+        ];
     });
